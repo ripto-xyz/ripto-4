377dab37307003d81917f05c8ce9e59bda9e62c0
haleslps <41848668-haleslps@users.noreply.replit.com>
2025-04-25 22:05:13 +0000

    Restored to 'cab5a717b95a28ee2cee136770d4785395556e1a'
    Replit-Restored-To: cab5a717b95a28ee2cee136770d4785395556e1a

--- client/src/components/YellowLogo.tsx
+++ client/src/components/YellowLogo.tsx
@@ -9,12 +9,29 @@ export default function YellowLogo({ className = '' }: YellowLogoProps) {
     <div className={`inline-block ${className}`}>
       <div className="text-lg md:text-xl font-black tracking-wide whitespace-nowrap perspective-[1000px]">
         {/* Add multiple shadow layers for enhanced 3D effect */}
-        {/* Using CSS shadows instead of multiple elements */}
-        <span className="yellow-logo-text uppercase inline-block tracking-wider">LAURENCE</span>
+        <div className="relative inline-block">
+          {/* Bottom shadow - deepest */}
+          <span className="yellow-logo-shadow-bottom uppercase absolute top-[3px] left-[1px] z-[-3]">LAURENCE</span>
+          {/* Left shadow */}
+          <span className="yellow-logo-shadow uppercase absolute top-0 left-0 z-[-2]">LAURENCE</span>
+          {/* Right-side shadow highlight */}
+          <span className="yellow-logo-shadow-right uppercase absolute top-0 left-[1px] z-[-1]">LAURENCE</span>
+          {/* Main text on top */}
+          <span className="yellow-logo-text uppercase inline-block">LAURENCE</span>
+        </div>
         
-        <span className="mx-1 md:mx-2 opacity-90 text-yellow-300 font-black animate-pulse">|</span>
+        <span className="mx-1 md:mx-2 opacity-90 text-yellow-300 font-black">|</span>
         
-        <span className="yellow-logo-text uppercase inline-block tracking-wider">RIPTO.ETH</span>
+        <div className="relative inline-block">
+          {/* Bottom shadow - deepest */}
+          <span className="yellow-logo-shadow-bottom uppercase absolute top-[3px] left-[1px] z-[-3]">RIPTO.ETH</span>
+          {/* Left shadow */}
+          <span className="yellow-logo-shadow uppercase absolute top-0 left-0 z-[-2]">RIPTO.ETH</span>
+          {/* Right-side shadow highlight */}
+          <span className="yellow-logo-shadow-right uppercase absolute top-0 left-[1px] z-[-1]">RIPTO.ETH</span>
+          {/* Main text on top */}
+          <span className="yellow-logo-text uppercase inline-block">RIPTO.ETH</span>
+        </div>
       </div>
     </div>
   );
